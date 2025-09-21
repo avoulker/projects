beedc4f1520b76f768800183a10b4de202741d49
avoulker <garickosandre@gmail.com>
2025-09-21 17:36:13 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,66 +1,128 @@
+#База, константи
+import pandas as pd
 import tkinter as tk
+from tkinter import ttk   #Імпортуємо бібліотеки
+
+top_10_gdp_per_capita_table = {
+    "Top 10 GDP per Capita (PPP, International Dollars)": ["Monaco($270,100)", "Liechtenstein($210,600)", "Singapore($132,600)", "Luxembourg($131,000)", "Ireland($115,300)", "Macau($112,800)", "Qatar($110,900)", "Bermuda($105,300)", "Norway($91,100)", "Switzerland($82,000)"]
+}
+
+top_10_gdp_nominal_table = {
+    "Top 10 nominal GDP (in Trillions USD)": ["USA($25,43T)", "China($14,72T)", "Japan($4,25T)", "Germany($3,85T)", "India($3,41T)", "United Kingdom($2,67T)", "France($2,63T)", "Russia($2,24T)", "Canada($2,16T)", "Italy($2,04T)"]
+}
+
+top_10_gdp_ppp_table = {
+    "Top 10 GDP (Purchasing Power Parity, in International Dollars)|": ["China($37.1T)", "USA($29,2T)", "India($16.0T)", "Russia($6.9T)", "Japan($6.6T)", "Germany($6.0T)", "Brazil($4.7T)", "Indonesia($4.7T)", "France($4.4T)", "United Kingdom($4.3T)"]
+}
+
+df_1 = pd.DataFrame(top_10_gdp_per_capita_table)
+df_2 = pd.DataFrame(top_10_gdp_nominal_table)
+df_3 = pd.DataFrame(top_10_gdp_ppp_table)    #Таблиці
+
+def show_frame(frame):
+    frame.tkraise()   #Піднімаємо вибраний екран(фрейм)
 
 root = tk.Tk()
-root.title("User manager")
+root.title("GDP of countries")
+root.attributes("-fullscreen", True)    #Робимо вікно
 
-def add_btn_handler():
-    name = name_entry.get().strip()
-    age = age_entry.get().strip()
+all_frames = tk.Frame(root)
+all_frames.pack(expand = True, fill = "both")
+all_frames.grid_rowconfigure(0, weight=1)
+all_frames.grid_columnconfigure(0, weight=1)    #Розміщуємо та налаштовуємо переміну в якій будуть всі екрани(фрейми)
 
-    if name and age:
-        users_list.insert(tk.END, f"{name} ({age})")
-        name_entry.delete(0, tk.END)
-        age_entry.delete(0, tk.END)
+main = tk.Frame(all_frames, bg = "white")
+top_10_gdp = tk.Frame(all_frames, bg = "white")
+top_10_gdp_per_capita = tk.Frame(all_frames, bg = "white")
+top_10_gdp_nominal = tk.Frame(all_frames, bg = "white")
+top_10_gdp_ppp = tk.Frame(all_frames, bg = "white")   #Екрани(фрейми)
 
-def update_btn_handler():
-    selected_index = users_list.curselection()
-    if selected_index:
-        name = name_entry.get().strip()
-        age = age_entry.get().strip()
-        if name and age:
-            users_list.delete(selected_index)
-            users_list.insert(selected_index, f"{name} ({age})")
-            name_entry.delete(0, tk.END)
-            age_entry.delete(0, tk.END)
+for frame in (main, top_10_gdp, top_10_gdp_per_capita, top_10_gdp_nominal, top_10_gdp_ppp):
+    frame.grid(row=0, column=0, sticky="nsew")   #Ростягуємо екран(фрейм)
 
-def delete_btn_handler():
-    selected_index = users_list.curselection()
-    if selected_index:
-        users_list.delete(selected_index)
 
-def on_select(event):
-    selected_index = users_list.curselection()
-    if selected_index:
-        user_text = users_list.get(selected_index)
-        if "(" in user_text and user_text.endswith(")"):
-            name = user_text[:user_text.rfind("(")].strip()
-            age = user_text[user_text.rfind("(")+1:-1].strip()
-            name_entry.delete(0, tk.END)
-            name_entry.insert(0, name)
-            age_entry.delete(0, tk.END)
-            age_entry.insert(0, age)
+#Кнопки, лейбли
+top_frame = tk.Frame(main, bg = "grey", height = 100)
+top_frame.pack(side = "top", fill = "both")   #Робимо верхню частину сірого кольору(main)
 
-# inputs:
-tk.Label(root, text="name: ").grid(row=0, column=0, padx=5, pady=5)
-name_entry = tk.Entry(root)
-name_entry.grid(row=0, column=1, padx=5, pady=5)
+label = tk.Label(top_frame, text = "Top 10 GDP countries in each category", font = ("Airal", 20), bg = "grey")
+label.pack()    #Розміщуємо текст(main(top_frame))
 
-tk.Label(root, text="age: ").grid(row=1, column=0, padx=5, pady=5)
-age_entry = tk.Entry(root)
-age_entry.grid(row=1, column=1, padx=5, pady=5)
+button = tk.Button(main, text = "View the top 10 GDP", font = ("Airal", 20), bg = "grey", command = lambda: show_frame(top_10_gdp))
+button.place(x = 100, y = 50)
+button = tk.Button(main, text = "View the chart of GDP growth over the last 25 years", font = ("Airal", 20), bg = "grey")
+button.place(x = 600, y = 50)
+button = tk.Button(main, text = "View the average values", font = ("Airal", 20), bg = "grey")
+button.place(x = 1500, y = 50)
+button = tk.Button(main, text = "Exit", font = ("Airal", 20), bg = "grey")
+button.place(x = 10, y = 1015)
+button = tk.Button(main, text = "Update log", font = ("Airal", 20), bg = "grey")
+button.place(x = 1400, y = 1015)
+button = tk.Button(main, text = "Setting", font = ("Airal", 20), bg = "grey")
+button.place(x = 1600, y = 1015)
+button = tk.Button(main, text = "FAQ", font = ("Airal", 20), bg = "grey")
+button.place(x = 1750, y = 1015)    #Розміщуємо кнопки(main)
 
-# buttons:
-add_btn = tk.Button(root, text="add", width=10, command=add_btn_handler)
-update_btn = tk.Button(root, text="update", width=10, command=update_btn_handler)
-delete_btn = tk.Button(root, text="delete", width=10, command=delete_btn_handler)
+top_frame = tk.Frame(top_10_gdp, top_10_gdp_per_capita,  bg = "grey", height = 38)
+top_frame.pack(side = "top", fill = "both")   #Робимо верхню частину сірого кольору(top_10_gdp)
 
-add_btn.grid(row=2, column=0, padx=5, pady=5)
-update_btn.grid(row=2, column=1, padx=5, pady=5)
-delete_btn.grid(row=2, column=2, padx=5, pady=5)
+label = tk.Label(top_frame, text = "Top 10 GDP", font = ("Airal", 20), bg = "grey")
+label.pack()    #Розміщуємо текст(top_10_gdp(top_frame))
 
-# users list:
-users_list = tk.Listbox(root, width=40)
-users_list.grid(row=3, column=0, columnspan=3, padx=5, pady=5)
-users_list.bind("<<ListboxSelect>>", on_select)
+button = tk.Button(top_10_gdp, text = "View top 10 GDP per capita (PPP, International Dollars)", font = ("Airal", 20), bg = "grey", command = lambda: show_frame(top_10_gdp_per_capita))
+button.place(x = 100, y = 50)
+button = tk.Button(top_10_gdp, text = "View top 10 nominal GDP (in Trillions USD))", font = ("Airal", 20), bg = "grey", command = lambda: show_frame(top_10_gdp_nominal))
+button.place(x = 1260, y = 50)
+button = tk.Button(top_10_gdp, text = "View top 10 GDP (Purchasing Power Parity, in International Dollars)", font = ("Airal", 20), bg = "grey", command = lambda: show_frame(top_10_gdp_ppp))
+button.place(x = 550, y = 115)
+button = tk.Button(top_10_gdp, text = "Back", font = ("Airal", 20), bg = "grey", command = lambda: show_frame(main))
+button.place(x = 10, y = 1015)
+button = tk.Button(top_10_gdp_per_capita, text = "Back", font = ("Airal", 20), bg = "grey", command = lambda: show_frame(top_10_gdp))
+button.place(x = 10, y = 1015)   #Розміщуємо кнопки(top_10_gdp) !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-root.mainloop()
+top_frame = tk.Frame(top_10_gdp_per_capita, bg = "grey", height = 38)
+top_frame.pack(side = "top", fill = "both")   #Робимо верхню частину сірого кольору(top_10_gdp_per_capita)
+
+label = tk.Label(top_frame, text = "Top 10 GDP per capita (PPP, International Dollars)", font = ("Airal", 20), bg = "grey")
+label.pack()    #Розміщуємо текст(top_10_gdp_per_capita(top_frame))
+
+
+#Таблиці
+table_1 = ttk.Treeview(top_10_gdp_per_capita, columns=list(df_1.columns), show="headings")   #Робимо таблицю(1)
+
+for col in df_1.columns:
+    table_1.heading(col, text=col)
+    table_1.column(col, width=300, anchor="center")
+
+for _, row in df_1.iterrows():
+    table_1.insert("", tk.END, values=list(row))
+
+table_1.pack(expand=True, fill="both")    #Задаємо параметри та розміщуємо таблицю(1)
+
+table_2 = ttk.Treeview(top_10_gdp_nominal, columns=list(df_2.columns), show="headings")   #Робимо таблицю
+
+for col in df_2.columns:
+    table_2.heading(col, text=col)
+    table_2.column(col, width=300, anchor="center")
+
+for _, row in df_2.iterrows():
+    table_2.insert("", tk.END, values=list(row))
+
+table_2.pack(expand=True, fill="both")    #Задаємо параметри та розміщуємо таблицю(2)
+
+table_3 = ttk.Treeview(top_10_gdp_ppp, columns=list(df_3.columns), show="headings")   #Робимо таблицю(3)
+
+for col in df_3.columns:
+    table_3.heading(col, text=col)
+    table_3.column(col, width=300, anchor="center")
+
+for _, row in df_3.iterrows():
+    table_3.insert("", tk.END, values=list(row))
+
+table_3.pack(expand=True, fill="both")    #Задаємо параметри та розміщуємо таблицю(2)
+
+
+#Виклик та ініціалізація
+show_frame(main)    #Викликаємо фунцію
+
+root.mainloop()   #Запуск вікна
